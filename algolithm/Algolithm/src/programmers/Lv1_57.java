b3062b31279b44b5bcf87a801c73f266285e1d85
jyseo <jyseo0901@naver.com>
2023-09-21 18:12:11 +0900

    update

---
+++ algolithm/Algolithm/src/programmers/Lv1_57.java
@@ -0,0 +1,16 @@
+package programmers;
+
+class Try57 {
+	public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+        return answer;
+    }
+}
+
+public class Lv1_57 {
+	public static void main(String[] args) {
+		Try57 T = new Try57();
+		int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
+		System.out.println(T.solution(5, stages));
+	}
+}
