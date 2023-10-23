22942d37a09dff53933f918682f42939ceff1212
JoonYoung2 <90246124+JoonYoung2@users.noreply.github.com>
2023-10-23 17:30:18 +0900

    231023 update

--- algolithm/Algolithm/src/programmers/level2/Lv2_16.java
+++ algolithm/Algolithm/src/programmers/level2/Lv2_16.java
@@ -1,10 +1,10 @@
 package programmers.level2;
 
 class Try16{
-	private static long cnt = 0;
 	private static int number = 0;
 	public long solution(int n) {
         long answer = 0;
+        long[] tmp = new long[2];
         if(n == 1) {
         	return 1;
         }else if(n == 2) {
@@ -12,29 +12,25 @@ class Try16{
         }else if(n == 3) {
         	return 3;
         }
-        number = n;
-        DFS(0);
-        answer = cnt;
+        number = 4;
+        tmp[0] = 2;
+        tmp[1] = 3;
+        while(number <= n) {
+        	long x = (tmp[0] % 1234567) + (tmp[1] % 1234567);
+        	tmp[0] = tmp[1];
+        	tmp[1] = x;
+        	number++;
+        }
+        
+        answer = tmp[1] % 1234567;
         return answer;
     }
-	
-	private void DFS(int n) {
-		if(n == number) {
-			cnt++;
-			return;
-		}
-		if(n > number) {
-			return;
-		}
-		DFS(n+1);
-		DFS(n+2);
-	}
 }
 
 public class Lv2_16 {
 	public static void main(String[] args) {
 		Try16 T = new Try16();
-		int n = 100;
+		int n = 2000;
 		System.out.println(T.solution(n));
 	}
 }
---
+++ algolithm/Algolithm/src/programmers/level2/Lv2_17.java
@@ -0,0 +1,17 @@
+package programmers.level2;
+
+class Try17{
+	public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        return answer;
+    }
+}
+
+public class Lv2_17 {
+	public static void main(String[] args) {
+		Try17 T = new Try17();
+		int k = 6;
+		int[] tangerine = {1, 3, 2, 5, 4, 5, 2, 3};
+		System.out.println(T.solution(k, tangerine));
+	}
+}
